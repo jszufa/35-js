b4c12b76cf3133dfe49ecc9b82643c59729d26cf
jszufa <jerzy.szufa@gmail.com>
2022-11-28 16:21:47 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+function check(n){
+    if (100 <= n && n <= 200) {
+        //console.log("Liczba znajduje się w przedziale");
+        return "Liczba znajduje się w przedziale"
+    }
+    else {
+        //console.log("Liczba nie znajduje się w przedziale");
+        return "Liczba nie znajduje się w przedziale"
+    }
+}
+
+//console.log(check(100));
